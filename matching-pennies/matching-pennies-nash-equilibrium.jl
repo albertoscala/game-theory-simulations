e31e5e5db3322294da971d09f22f2928fb9fa555
Alberto Scala <alberto.scala.privata@gmail.com>
2023-12-20 12:20:01 +0100

    finished first matching pennies

--- matching-pennies/matching-pennies-nash-equilibrium.jl
+++ matching-pennies/matching-pennies-nash-equilibrium.jl
@@ -9,38 +9,20 @@ for player in players
     player["strategy"] = rand(0:1)
 end
 
-# Choose coin side for each player
-for player in players
-    player["coin"] = rand(0:1)
-end
-
 # Create payoff matrix
 payoff_matrix = [
     [(1, -1), (-1, 1)],
     [(-1, 1), (1, -1)]
 ]
 
-# Calculate points for each player
-if players[1]["coin"] == players[2]["coin"]
-    if players[1]["strategy"] == 0
-        players[1]["points"] += 1
-    else
-        players[1]["points"] -= 1
-    end
-    if players[1]["strategy"] == 0
-        players[1]["points"] += 1
-    else
-        players[1]["points"] -= 1
-    end
-else
-    if players[1]["strategy"] == 1
-        players[1]["points"] += 1
-    else
-        players[1]["points"] -= 1
-    end
-    if players[1]["strategy"] == 1
-        players[1]["points"] += 1
-    else
-        players[1]["points"] -= 1
-    end
+# Choose coin side for each player
+for player in players
+    player["coin"] = rand(0:1)
 end
+
+# Calculate points for each player
+players[1]["points"] = payoff_matrix[players[1]["coin"]+1][players[2]["coin"]+1][players[1]["strategy"]+1]
+players[2]["points"] = payoff_matrix[players[1]["coin"]+1][players[2]["coin"]+1][players[2]["strategy"]+1]
+
+println("Player 1: $(players[1])")
+println("Player 2: $(players[2])")
